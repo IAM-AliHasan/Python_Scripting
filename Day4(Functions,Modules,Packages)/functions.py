51ba5450f79cecaa84459b930580914fc5fd58ae
IAM-AliHasan <168626353+IAM-AliHasan@users.noreply.github.com>
2024-08-19 11:10:24 +0500

    Update functions.py

--- Day4(Functions,Modules,Packages)/functions.py
+++ Day4(Functions,Modules,Packages)/functions.py
@@ -1,1 +1,17 @@
+def add(num1,num2):
+    x=num1+num2
+    return x
 
+def mul(num1,num2):
+    x=num1*num2
+    return x
+def divide(num1,num2):
+    x=num1/num2
+    return x
+
+y=add(3,3)
+print("the addition of two numbers is:",y)
+w=mul(2,3)
+print("the multiplication of two numbers is :",w)
+z=divide(12,2)
+print("the division of two numbers is :",z)
