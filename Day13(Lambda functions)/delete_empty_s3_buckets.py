a2ffeb4c98c25bec5d9f599500231a85f45ba59e
IAM-AliHasan <168626353+IAM-AliHasan@users.noreply.github.com>
2024-08-26 21:31:23 +0500

    Update delete_empty_s3_buckets.py

--- Day13(Lambda functions)/delete_empty_s3_buckets.py
+++ Day13(Lambda functions)/delete_empty_s3_buckets.py
@@ -1,1 +1,29 @@
+import boto3
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    
+    for bucket in response['Buckets']:
+        bucket_name = bucket['Name']
+        objects = s3.list_objects_v2(Bucket=bucket_name)
+        
+        if 'Contents' not in objects:
+            try:
+                # Delete all versions of all objects in the bucket
+                versions = s3.list_object_versions(Bucket=bucket_name)
+                if 'Versions' in versions:
+                    for version in versions['Versions']:
+                        s3.delete_object(Bucket=bucket_name, Key=version['Key'], VersionId=version['VersionId'])
+                if 'DeleteMarkers' in versions:
+                    for marker in versions['DeleteMarkers']:
+                        s3.delete_object(Bucket=bucket_name, Key=marker['Key'], VersionId=marker['VersionId'])
+                
+                # Delete the bucket
+                s3.delete_bucket(Bucket=bucket_name)
+                print(f"Deleted bucket: {bucket_name}")
+            except Exception as e:
+                print(f"Error deleting bucket {bucket_name}: {e}")
+        else:
+            print(f"Bucket {bucket_name} is not empty, skipping deletion.")
 
